fc9deb96b11ebdea424addf5b525e928ffbfdb8b
Vernigoroff <lesha_vrn@bk.ru>
2020-12-11 15:34:22 +0300

    homework2 JS BASIC

---
+++ homework2/Math__operations(4)/Math__operations.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/**
+ * Функция принимает параметры a и b, возвращает их сумму.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+
+function sum(a, b) {
+    return a + b;
+}
+
+
+
+/**
+* Функция принимает параметры a и b, возвращает их разность.
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+
+function difference(a, b) {
+    return a - b;
+}
+
+
+
+/**
+* Функция принимает параметры a и b, возвращает их произведение.
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+
+function composition(a, b) {
+    return a * b;
+}
+
+
+
+/**
+* Функция принимает параметры a и b, возвращает деление a на b.
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+
+function division(a, b) {
+    return a / b;
+}
